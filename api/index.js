0d262bbad4c581caa8d50e520e71442977543bc2
sergykt <sergykt1992@mail.ru>
2023-09-07 00:33:02 +0600

    little fixes

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@
 const Express = require('express');
 const cors = require('cors');
 
-const questions = require('../src/questions');
+const questions = require('./src/questions');
 
 const getLastId = () => questions[questions.length - 1].id;
 
+const apiPath = '/api';
+
 const routes = {
-  data: '/api/data',
+  data: () => [apiPath, 'data'].join('/'),
 };
 
 const app = new Express();
@@ -17,20 +19,28 @@ app.use(cors())
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
-app.get(routes.data, (req, res) => {
+app.get(routes.data(), (req, res) => {
   res.json(questions);
-  res.end();
 });
 
-app.post(routes.data, (req, res) => {
+app.post(routes.data(), (req, res) => {
   const id = getLastId() + 1;
   const { body } = req;
-  const { text, option1, option2, answer, recommendation } = body;
+  const {
+    text,
+    option1,
+    option2,
+    answer,
+    recommendation
+  } = body;
   const data = { text, options: [option1, option2], answer, recommendation, id };
   questions.push(data);
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.json(data);
-  res.end();
 });
 
-app.listen(5005);
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
