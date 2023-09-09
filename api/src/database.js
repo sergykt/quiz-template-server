62b403e06a432d0643b76a1971ac12abd9c226e5
sergykt <sergykt1992@mail.ru>
2023-09-10 00:30:48 +0600

    changed sql path

--- api/src/database.js
+++ api/src/database.js
@@ -4,6 +4,8 @@ const password = '83hivofo';
 const host = 'localhost';
 const port = 5432;
 
-const dbPath = process.env.NODE_ENV === 'production' ? 'postgres://serg:Fl9qQ2C10uibfWSo0edYXKyNadafeDOa@dpg-cjub0vnhdsdc73ekctug-a.oregon-postgres.render.com/quiz_template_server_sql' : `postgres://${username}:${password}@${host}:${port}/${dbName}`;
+//const dbPath = process.env.NODE_ENV === 'production' ? 'postgres://serg:Fl9qQ2C10uibfWSo0edYXKyNadafeDOa@dpg-cjub0vnhdsdc73ekctug-a.oregon-postgres.render.com/quiz_template_server_sql' : `postgres://${username}:${password}@${host}:${port}/${dbName}`;
+
+const dbPath = 'postgres://serg:Fl9qQ2C10uibfWSo0edYXKyNadafeDOa@dpg-cjub0vnhdsdc73ekctug-a.oregon-postgres.render.com/quiz_template_server_sql';
 
 module.exports = dbPath;
