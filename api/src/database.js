d93595318cbae32a7b6ad8a2a57344e51b31dacc
sergykt <sergykt1992@mail.ru>
2023-09-09 23:46:45 +0600

    changed sql path

--- api/src/database.js
+++ api/src/database.js
@@ -4,6 +4,6 @@ const password = '83hivofo';
 const host = 'localhost';
 const port = 5432;
 
-const dbPath = process.env.NODE_ENV === 'production' ? 'postgres://default:gBGiZTOL96dw@ep-small-dawn-11427518.us-east-1.postgres.vercel-storage.com:5432/verceldb' : `postgres://${username}:${password}@${host}:${port}/${dbName}`;
+const dbPath = process.env.NODE_ENV === 'production' ? 'postgres://default:gBGiZTOL96dw@ep-small-dawn-11427518-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb' : `postgres://${username}:${password}@${host}:${port}/${dbName}`;
 
 module.exports = dbPath;
