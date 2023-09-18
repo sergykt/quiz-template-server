e8199d43128c15ca274e57d5c8c3217f4c356b1f
sergykt <sergykt1992@mail.ru>
2023-09-18 18:58:00 +0600

    check token middleware

--- api/middlewares/middlewares.js
+++ api/middlewares/middlewares.js
@@ -15,7 +15,8 @@ const authMiddleware = (req, res, next) => {
     next();
   }
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.Authorization?.split(' ')[1];
+    console.log(token);
     if (!token) {
       console.error(`[${getCurrentTime()}] Пользователь не авторизован`);
       return res.status(401).end();
