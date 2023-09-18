cb629fed7b350bb4b52b86a0178c28bb4d1f2723
sergykt <sergykt1992@mail.ru>
2023-09-18 19:42:31 +0600

    fix clearcookie

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -215,7 +215,7 @@ class UserController {
       const { user_id: userId } = token;
       await tokenModel.delete(userId);
       console.log(`[${getCurrentTime()}] Выполнен выход пользователя с ID: ${userId}`);
-      res.clearCookie('refreshToken');
+      res.clearCookie('refreshToken', { sameSite: 'none' });
       return res.status(200).end();
     } catch (err) {
       console.error(`[${getCurrentTime()}] Произошла ошибка при выходе пользователя ${err}.`);
